75af32d87d114bae72f82619ac21db4f8e083c84
luckyboyxu <35586965+luckyboyxu@users.noreply.github.com>
2018-01-25 10:37:52 +0800

    Update aide_report.sh

--- bin/aide_report.sh
+++ bin/aide_report.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #Program:
-#        Detect the changed system files.
+#    Detect the changed system files.
 #History:
-#        2009.8.31   zhoubo     First release
+#    2009.8.31   zhoubo     First release
 
 
 declare -i date_time=`date +%Y%m%d%H`
 
 cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+#aide是个入侵检测工具，主要用途是检查文本的完整性
 /usr/sbin/aide -i
 cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide_backup/aide.db.new.gz_$date_time
 /usr/sbin/aide --compare > /var/lib/aide/aide_report/aide_report_$date_time
